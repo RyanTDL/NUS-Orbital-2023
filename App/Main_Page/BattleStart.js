5d393883c192e538b391d92b637b7c4333f8d0b4
Yogee-s <yogeeswaran@nus.edu.sg>
2023-06-20 23:24:08 +0800

    Cleaned up the code

--- App/Main_Page/BattleStart.js
+++ App/Main_Page/BattleStart.js
@@ -8,6 +8,11 @@ const {width, height}= Dimensions.get('window'); //retrieves dimensions of the s
 
 export default function BattlePage({navigation, route}) {
 
+    //Heal button
+    const stamina = parseInt(route.params.friendinfo[4]);
+    console.log(`original stamina is ${stamina}`);
+    const [healstat, sethealstat] = useState(stamina);
+
     //Load Font 
     const [loaded] = useFonts({
         'PressStart2P-Regular': require('../../assets/fonts/PressStart2P-Regular.ttf'),      
@@ -16,139 +21,104 @@ export default function BattlePage({navigation, route}) {
         return null;
     }
 
-    let friend_icon = route.params.friendinfo[1] ;
-    console.log(friend_icon);
+    const friendIcon = route.params.friendinfo[1] ;
+    console.log(friendIcon);
 
-    //Heal button (Work in progress)
-    let healthstat = parseInt(route.params.friendinfo[4]);
-    // const [healthstat, sethealthstat] = useState(parseInt(route.params.friendinfo[4]));
+
+    const healClick = () => {
+        sethealstat(healstat+50);
+    }
 
     return(
         <SafeAreaView style={styles.container}>
-            <View style={styles.players_info_container}>
-       
-                <View style={styles.player_info}> 
-                    <ImageBackground
-                  source={require('../../assets/battlesystem/playerholder.jpg')}
-                  style={styles.player_info}
-                  > 
+            <View style={styles.playersInfoContainer}>
 
-                  <View style={styles.playeridentity}>
-                      <Image source={require('../../assets/player_avatars/star_athlete.png')}/>
-                      <Text style={styles.playername}>
-                      User203
-                      </Text>
-                  </View> 
-                  
-                  <View style={styles.playerstats}>
-                      <View style={styles.playerstats_child}>
-                          <View style={styles.playerstatsicon}>
-                              <Image
-                                  style={{
-                                      flex: 1,
-                                      width: 30,
-                                      resizeMode: 'contain',
-                                  }}
-                                  source={require('../../assets/battlesystem/heart.png')}
-                                  />
+                <View style={styles.playerInfo}> 
+                    <ImageBackground style={styles.infoBackground}
+                        source={require('../../assets/battlesystem/playerInfoBackground.jpg')}
+                        > 
 
-                          </View>
-                          
+                        <View style={styles.playerIcon}>
+                            <Image source={require('../../assets/player_avatars/star_athlete.png')}/>
+                            <Text style={styles.playerName}>
+                            User203
+                            </Text>
+                        </View> 
 
-                          <View style={styles.statusbar}>
-                            <View style ={[styles.statusbar,{ backgroundColor:'#fc080d', width: healthstat}]}>
+                        <View style={{flex: 2}}>
+                            <View style={styles.playerStats}>
+                                <View style={styles.playerStatsIcon}>
+                                    <Image
+                                        style={{flex: 1, width: 30, resizeMode: 'contain',}}
+                                        source={require('../../assets/battlesystem/heart.png')}
+                                        />
+                                </View>
                                 
+                                <View style={styles.statusbar}>
+                                    <View style ={[styles.statusbar,{ backgroundColor:'#fc080d', width: healstat }]}>
+                                    </View>
+                                </View>
                             </View>
-                          </View>
 
-                      </View>
+                            <View style={styles.playerStats}> 
+                                <View style={{flex: 0.2}}>
+                                    <Image
+                                        style={{flex: 1, width: 30, resizeMode: 'contain',}}
+                                        source={require('../../assets/battlesystem/power.png')}
+                                        />
+                                </View>
 
-                      <View style={styles.playerstats_child}> 
-                          <View style={styles.playerstatsicon}>
-                              <Image
-                                  style={{
-                                      flex: 1,
-                                      width: 30,
-                                      resizeMode: 'contain',
-                                  }}
-                                  source={require('../../assets/battlesystem/power.png')}
-                                  />
-                          </View>
-
-                          <View style={styles.statusbar}>
-                            <View style ={[styles.statusbar,{backgroundColor:'#D5B71C', width: parseInt(route.params.friendinfo[5])}]}>
-                                
+                                <View style={styles.statusbar}>
+                                    <View style ={[styles.statusbar,{backgroundColor:'#D5B71C', width: '100%'}]}>
+                                    </View>
+                                </View>
                             </View>
-                          </View>
-
-                      </View>
-                  </View>
+                        </View>
                     </ImageBackground>
-
                 </View>
 
-                <View style={styles.player_info}> 
-                <ImageBackground
-                  source={require('../../assets/battlesystem/playerholder.jpg')}
-                  style={styles.player_info}
-                  > 
+                <View style={styles.playerInfo}> 
+                    <ImageBackground style={styles.infoBackground}
+                        source={require('../../assets/battlesystem/playerInfoBackground.jpg')}
+                        > 
 
-                  <View style={styles.playeridentity}>
-                  <Image source={require('../../assets/player_avatars/star_athlete.png')}/>
-                  {/* <Image source={require({friend_icon})}/> */}
+                        <View style={styles.playerIcon}>
+                            <Image source={require('../../assets/player_avatars/star_athlete.png')}/>
+                            <Text style={styles.playerName}>
+                            {route.params.friendinfo[0]}
+                            </Text>
+                        </View> 
 
-
-                      <Text style={styles.playername}>
-                      {route.params.friendinfo[0]}
-                      </Text>
-                  </View> 
-                  
-                  <View style={styles.playerstats}>
-                      <View style={styles.playerstats_child}>
-                          <View style={styles.playerstatsicon}>
-                              <Image
-                                  style={{
-                                      flex: 1,
-                                      width: 30,
-                                      resizeMode: 'contain',
-                                  }}
-                                  source={require('../../assets/battlesystem/heart.png')}
-                                  />
-
-                          </View>
-                          
-
-                          <View style={styles.statusbar}>
-                            <View style ={[styles.statusbar,{backgroundColor:'#fc080d', width: parseInt(route.params.friendinfo[4])}]}>
+                        <View style={{flex: 2}}>
+                            <View style={styles.playerStats}>
+                                <View style={styles.playerStatsIcon}>
+                                    <Image
+                                        style={{flex: 1, width: 30, resizeMode: 'contain',}}
+                                        source={require('../../assets/battlesystem/heart.png')}
+                                        />
+                                </View>
                                 
+                                <View style={styles.statusbar}>
+                                    <View style ={[styles.statusbar,{ backgroundColor:'#fc080d', width: '100%' }]}>
+                                    </View>
+                                </View>
                             </View>
-                          </View>
 
-                      </View>
+                            <View style={styles.playerStats}> 
+                                <View style={{flex: 0.2}}>
+                                    <Image
+                                        style={{flex: 1, width: 30, resizeMode: 'contain',}}
+                                        source={require('../../assets/battlesystem/power.png')}
+                                        />
+                                </View>
 
-                      <View style={styles.playerstats_child}> 
-                          <View style={styles.playerstatsicon}>
-                              <Image
-                                  style={{
-                                      flex: 1,
-                                      width: 30,
-                                      resizeMode: 'contain',
-                                  }}
-                                  source={require('../../assets/battlesystem/power.png')}
-                                  />
-                          </View>
-
-                          <View style={styles.statusbar}>
-                            <View style ={[styles.statusbar,{backgroundColor:'#D5B71C', width: parseInt(route.params.friendinfo[5])}]}>
-                                
+                                <View style={styles.statusbar}>
+                                    <View style ={[styles.statusbar,{backgroundColor:'#D5B71C', width: '100%'}]}>
+                                    </View>
+                                </View>
                             </View>
-                          </View>
-
-                      </View>
-                  </View>
+                        </View>
                     </ImageBackground>
-
-
                 </View>
 
 
@@ -156,49 +126,45 @@ export default function BattlePage({navigation, route}) {
 
             <View style={styles.animationwindow}>
                 <Image 
-                source={require('../../assets/battlesystem/battlebackground.jpg')}
-                style={styles.battlebackgroundimage}
+                    style={styles.battlebackgroundimage}
+                    source={require('../../assets/battlesystem/battlebackground.jpg')}
                 />
             </View>
+
             <View style={styles.infobox}>
                 <Text style={[styles.text, {textAlign: "left", paddingLeft: 15}]}> 
                     Choose your next move!
                 </Text>
             </View>
 
-
             <View style={styles.inputbox}>
 
-                <View style={styles.moves_container}>
-                    <Pressable style={[styles.moves_button, {backgroundColor: '#fc080d',orderColor: '#8e0000'}]}  
+                <View style={styles.movesContainer}>
+                    <Pressable style={[styles.movesButton, {backgroundColor: '#fc080d',borderColor: '#8e0000'}]}  
                     onPress = {() => console.log('Attack')}>
-                        <Text style={styles.text}> ATTACK </Text>
+                        <Text style={[styles.text, {fontSize: 21}]}> ATTACK </Text>
                     </Pressable>
 
-                    <Pressable style={[styles.moves_button, {backgroundColor: '#D5B71C',orderColor: '#635f09'}]} 
+                    <Pressable style={[styles.movesButton, {backgroundColor: '#D5B71C',borderColor: '#635f09'}]} 
                     onPress = {() => console.log('ULTIMATE')}>
-                        <Text style={[styles.text, {fontSize: 19}]}> ULTIMATE </Text>
+                        <Text style={[styles.text, {fontSize: 17}]}> ULTIMATE </Text>
                     </Pressable>
                 </View>
 
-                <View style={styles.moves_container}>
-                    <Pressable style={[styles.moves_button, {backgroundColor: '#61A631',orderColor: '#0E6600'}]} 
-                    // onPress = {() => console.log('HEAL')}>   
+                <View style={styles.movesContainer}>
+                    <Pressable style={[styles.movesButton, {backgroundColor: '#61A631',borderColor: '#0E6600' }]}
 
-                    onPress={ () => {
-                    return (
-                        console.log('HEAL'),
-                        healthstat += 10
-                    )}
-
-                }>
-                        <Text style={styles.text}> HEAL </Text>
-
-
+                    onPress={() => {
+                        console.log('HEAL');
+                        healClick();
+                        console.log(`updated ${healstat}`);
+                      }}
+                    > 
+                        <Text style={styles.text} > HEAL </Text>
                         </Pressable>
 
 
-                    <Pressable style={[styles.moves_button, {backgroundColor: '#76C4E8',orderColor: '#0098BA'}]} 
+                    <Pressable style={[styles.movesButton, {backgroundColor: '#76C4E8',borderColor: '#0098BA'}]} 
                     onPress={() => {
                         console.log('RUN');
                         navigation.navigate('Friends List');
@@ -216,61 +182,48 @@ const styles = StyleSheet.create({
     container: {
         paddingTop: StatusBar.currentHeight,
         flex: 1,
-        backgroundColor: 'black', 
-        
+        backgroundColor: 'black',   
     },
 
-    players_info_container: {
+    playersInfoContainer: {
         flex: 0.7,
-        backgroundColor: 'black',
         flexDirection: 'row', 
     },
 
-    player_info: {
+    playerInfo: {
         flex: 1,
         borderWidth: 2,
         borderColor: 'black',
         borderRadius: 20,       
     },
 
-
-    playeridentity: {
-        //playericon
-        flex: 3,
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        paddingTop: 10,
+    infoBackground: {
+        flex: 1,
+        borderRadius: 10,
+        overflow: 'hidden',
 
     },
 
+    playerIcon: {
+        flex: 3,
+        alignItems: 'center',
+        paddingTop: 10,
+    },
 
-    playername: {
+    playerName: {
         flex: 1,
         fontWeight: '400',
-        fontSize: 25,
+        fontSize: 28,
         textAlign: 'center',
         textAlignVertical: 'bottom',
         color: '#FFFFFF',
-        textShadowColor: '#000000',
-        textShadowOffset: { width: 2, height: 4 },
-        textShadowRadius: 4,      
     },
 
-    playerstats: {
-        flex: 2,
-        flexDirection: 'column',
-        // borderColor: 'red',
-        // borderWidth: 5,
-    },
-    playerstats_child: {
+    playerStats: {
         flex: 1,
         flexDirection: 'row',
     },
 
-    playerstatsicon: {
-        flex: 0.2,
-    },
 
     statusbar: {
         flex: 1,
@@ -280,6 +233,7 @@ const styles = StyleSheet.create({
         borderRadius: 5, 
         borderColor:'black', 
         backgroundColor: 'grey',
+        marginEnd: 8,
         
     },
 
@@ -319,23 +273,18 @@ const styles = StyleSheet.create({
         boxSizing: 'border-box',
       },
 
-    moves_container: {
+    movesContainer: {
         flex: 1,
         flexDirection: "row",
-        justifyContent: 'space-evenly',
     },
 
-    moves_button:{
+    movesButton:{
         flex: 1,
-        borderWidth: 5,
+        margin: 3,
+        borderWidth: 8,
         borderRadius: 15,
-        shadowColor: 'rgba(0, 0, 0, 0.25)',
-        shadowOffset: { width: 0, height: 4 },
-        shadowOpacity: 1,
-        shadowRadius: 10,
-        elevation: 10,
+        alignItems: 'center',
         justifyContent: 'center',
-
     },
 
     text:{
@@ -343,12 +292,10 @@ const styles = StyleSheet.create({
         fontWeight: '400',
         fontSize: 25,
         lineHeight: 30,
-        textAlign: 'center',
         color: '#FFFFFF',
         textShadowColor: '#000000',
-        textShadowOffset: { width: 2, height: 3.5 },
+        textShadowOffset: { width: 2.2, height: 3.5 },
         textShadowRadius: 4,
-    
     },
 
 })
